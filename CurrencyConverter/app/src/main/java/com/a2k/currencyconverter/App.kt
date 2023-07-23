2d5209f34ab7a08ae38c62c5c995b00a6a84e642
a2krocks <aakapurv@gmail.com>
2023-07-23 16:04:54 +0530

    Updated UI

--- CurrencyConverter/app/src/main/java/com/a2k/currencyconverter/App.kt
+++ CurrencyConverter/app/src/main/java/com/a2k/currencyconverter/App.kt
@@ -1,7 +1,13 @@
 package com.a2k.currencyconverter
 
 import android.app.Application
+import com.google.android.material.color.DynamicColors
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App: Application()
+class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        DynamicColors.applyToActivitiesIfAvailable(this)
+    }
+}
--- CurrencyConverter/app/src/main/java/com/a2k/currencyconverter/MainActivity.kt
+++ CurrencyConverter/app/src/main/java/com/a2k/currencyconverter/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.a2k.currencyconverter
 
+import android.R
+import android.icu.util.Currency
+import android.icu.util.CurrencyAmount
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.isVisible
+import androidx.core.view.updatePadding
 import androidx.lifecycle.lifecycleScope
 import com.a2k.currencyconverter.databinding.ActivityMainBinding
 import com.a2k.currencyconverter.main.MainViewModel
@@ -23,13 +34,36 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        window.navigationBarDividerColor =
+            ContextCompat.getColor(this, android.R.color.transparent)
+        window.navigationBarColor = ContextCompat.getColor(this, android.R.color.transparent)
+        window.statusBarColor = ContextCompat.getColor(this, android.R.color.transparent)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        setSupportActionBar(binding.toolbar)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.toolbar) { view, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.updatePadding(top = insets.top)
+            WindowInsetsCompat.CONSUMED
+        }
+
+
+        binding.from.setText("INR", false)
+        binding.to.setText("USD", false)
+
+        binding.from.setOnItemClickListener { adapterView, view, i, l ->
+            binding.valueContainer.prefixText =  CurrencyAmount(
+                0,
+                Currency.getInstance(binding.from.text.toString())
+            ).currency.symbol.toString()
+        }
+
 
         binding.conBtn.setOnClickListener {
 
             viewModel.convert(
                 binding.value.text.toString(),
-                binding.from.selectedItem.toString(),
-                binding.to.selectedItem.toString(),
+                binding.from.text.toString(),
+                binding.to.text.toString(),
             )
         }
 
@@ -37,9 +71,12 @@ class MainActivity : AppCompatActivity() {
             viewModel.conversion.collect { event ->
                 when (event) {
                     is MainViewModel.CurrencyEvent.Success -> {
-
+                        val currency =  CurrencyAmount(
+                            event.resultText.toDouble(),
+                            Currency.getInstance(binding.to.text.toString())
+                        )
                         binding.progressBar.isVisible = false
-                        binding.result.setText(event.resultText)
+                        binding.result.text = "${currency.currency.symbol} ${currency.number}"
                     }
 
                     is MainViewModel.CurrencyEvent.Failure -> {
